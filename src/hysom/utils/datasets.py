c0e690da3040d9bb906c8f660260e15217ad93f5
ArlexMR <arlexmarinr@gmail.com>
2025-11-05 16:07:56 -0500

    load General_TQ_SOM from local file

--- src/hysom/utils/datasets.py
+++ src/hysom/utils/datasets.py
@@ -1,30 +1,10 @@
 import json
 import numpy as np 
 from importlib import resources
-import urllib.request
-from datetime import datetime
 
-
-__sample_data_url = "https://raw.githubusercontent.com/ArlexMR/HySOM/refs/heads/main/src/hysom/data/classified_loops.json"
 __QT_watershed_01191000_filename = "QT_01191000.json"
 __events_watershed_01191000_filename = "events_01191000.json"
 
-
-def fetch_json(url) -> dict: 
-    """Fetches a JSON file from a URL and returns it as a Python dictionary."""
-    try:
-        with urllib.request.urlopen(url) as response:
-            data = response.read().decode("utf-8")  # Read and decode the response
-            return json.loads(data)  # Parse the JSON string into a dictionary
-    except Exception as e:
-        print(f"Error fetching JSON: {e}")
-        return {}
-
-
-def get_sample_data():
-
-    data = fetch_json(__sample_data_url)
-    return np.array(data["arrays"])
 
 def get_labeled_loops():
     ref = resources.files("hysom.data")
